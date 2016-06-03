db2ae6b0a38c7ca4d00873d1ae941e7b7c1e884f
erjainam <engineerjainam@gmail.com>
2016-06-03 16:39:45 +0530

    added divider in recyclerview

--- app/src/main/java/com/rishabhjain/jvocsportscorner/ClubMembersFragment.java
+++ app/src/main/java/com/rishabhjain/jvocsportscorner/ClubMembersFragment.java
@@ -2,10 +2,7 @@ package com.rishabhjain.jvocsportscorner;
 
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,7 +10,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -39,6 +35,7 @@ public class ClubMembersFragment extends Fragment {
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext()));
         return view;
     }
 
